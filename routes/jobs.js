fdee6c81bacb8af485d594c4e2e5b19a71c79357
Denise Muniz <dendenmuniz@gmail.com>
2024-04-17 15:32:44 +0200

    protect routes from unauthorized users

--- routes/jobs.js
+++ routes/jobs.js
@@ -11,11 +11,13 @@ const {
   jobStats,
 } = require("../controllers/jobsController");
 
+const { isAuthenticadedUser } = require("../middlewares/auth");
+
 router.route("/jobs").get(getJobs);
 router.route("/job/:id/:slug").get(getJob);
 router.route("/jobs/:zipcode/:distance").get(getJobsInRadius);
-router.route("/job/new").post(newJob);
-router.route("/job/:id").put(updateJob).delete(deleteJob);
+router.route("/job/new").post(isAuthenticadedUser, newJob);
+router.route("/job/:id").put(isAuthenticadedUser,updateJob).delete(isAuthenticadedUser,deleteJob);
 router.route("/stats/:topic").get(jobStats);
 
 module.exports = router;
