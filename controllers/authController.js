9f2008e1be47a4be79de834f05137099e7b4bb85
Denise Muniz <dendenmuniz@gmail.com>
2024-04-17 10:02:56 +0200

    create login user and assign token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/users");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const ErrorHandler = require("../utils/errorHandler");
 
 //Register a new user -> /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -19,4 +20,31 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     message: "User registered successfully",
     token,
   });
+});
+
+//login user -> /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //checks if email and password are informed
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter your credencials"), 400);
+  }
+
+  //Finding user
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid credentials", 401));
+  }
+
+  //Check password
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid credentials", 401));
+  }
+
+  //Creat Json web token
+  const token = user.getJwtToken();
+  res.status(200).json({ sucess: true, token: token });
 });
--- models/users.js
+++ models/users.js
@@ -49,4 +49,9 @@ userSchema.methods.getJwtToken = function () {
   });
 };
 
+//Compare password
+userSchema.methods.comparePassword = async function (enterPassword) {
+  return await bcrypt.compare(enterPassword, this.password);
+};
+
 module.exports = mongoose.model("User", userSchema);
